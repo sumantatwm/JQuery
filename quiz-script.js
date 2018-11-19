a181ae4b171cbfd8e35b0ee7b30ad05fd6ea170f
sumantatwm <sumantatwm@gmail.com>
2018-11-19 17:00:13 +0530

    updated with random questions & pie chart..

--- quiz-script.js
+++ quiz-script.js
@@ -1,6 +1,16 @@
-var currentQuestion = 0;
+console.log("connected");
+var randomquestionIndex = [];
+while (randomquestionIndex.length<5) {
+    var i = Math.floor(Math.random()*10) + 1;
+    if (randomquestionIndex.indexOf(i) === -1) {
+        randomquestionIndex.push(i);
+    }
+}
+console.log(randomquestionIndex);
 var score = 0;
-var totalQuestions= questions.length;//Math.floor(Math.random() * 5) + 2 ;
+var totalQuestions= 5;//questions.length;//Math.floor(Math.random() * 5) + 2 ;
+var indexCount = 0;
+var currentQuestion = randomquestionIndex[indexCount];
 
 var container = document.getElementById("quizContainer");
 var questionEl = document.getElementById("questions");
@@ -12,10 +22,11 @@ var nextButton = document.getElementById("nextButton");
 var againButton = document.getElementById("againButton");
 var resultContainer = document.getElementById("result");
 var nextButtonContainer = document.getElementById("nextQuiz");
+var correctQuestionCount = 0;
 
-function loadQuestion(questionIndex) {
+function loadQuestion(questionIndex,i) {
     var q = questions[questionIndex];
-    questionEl.textContent = (questionIndex + 1) + '. ' +q.question;
+    questionEl.textContent = (i+1) + '. ' +q.question;
     opt1.textContent = q.option1;
     opt2.textContent = q.option2;
     opt3.textContent = q.option3;
@@ -29,24 +40,49 @@ function loadNextQuestion() {
         return;
     }
     var answer = selectedOption.value;
-    if (questions[currentQuestion].answer ==answer) {
+    if (questions[currentQuestion].answer == answer) {
         this.score = this.score+1;
+        this.correctQuestionCount = this.correctQuestionCount+1;
     }
 
     selectedOption.checked = false;
-    currentQuestion++;
+    indexCount++;
+    currentQuestion = randomquestionIndex[indexCount];
 
-    if (currentQuestion == totalQuestions-1) {
+    if (indexCount == totalQuestions-1) {
         nextButton.textContent = "Finish";
     }
-    if (currentQuestion == totalQuestions) {
+    if (indexCount == totalQuestions) {
         container.style.display = 'none';
         resultContainer.style.display = '';
-        resultContainer.textContent = "Your Score: "+this.score+" out of "+totalQuestions;
+
+        //Google Pie Chart
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+            ['Scores', 'Out Of 5'],
+            ['correct', this.correctQuestionCount],
+            ['Wrong', this.totalQuestions-this.correctQuestionCount]
+
+          ]);
+
+        // Optional; add a title and set the width and height of the chart
+        var options = {'title':'Your Score: '+this.score+' out of '+totalQuestions, 'width':550, 'height':400};
+
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.PieChart(resultContainer);
+        chart.draw(data, options);
+
+        }
+
+        
+
+       // resultContainer.textContent = "Your Score: "+this.score+" out of "+totalQuestions;
         return;
     }
 
-    loadQuestion(currentQuestion);
+    loadQuestion(currentQuestion,indexCount);
 }
 
-loadQuestion(currentQuestion);
+loadQuestion(currentQuestion,indexCount);
