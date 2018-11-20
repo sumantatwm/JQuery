91f47f6614bbf4d9c26ab42085d371b846055c48
sumantatwm <sumantatwm@gmail.com>
2018-11-20 16:45:48 +0530

    updated with new json logic

--- quiz-script.js
+++ quiz-script.js
@@ -1,7 +1,7 @@
 console.log("connected");
 var randomquestionIndex = [];
 while (randomquestionIndex.length<5) {
-    var i = Math.floor(Math.random()*10) + 1;
+    var i = Math.floor(Math.random()*9) + 0;
     if (randomquestionIndex.indexOf(i) === -1) {
         randomquestionIndex.push(i);
     }
@@ -18,6 +18,10 @@ var option1 = document.getElementById("opt1");
 var option2 = document.getElementById("opt2");
 var option3 = document.getElementById("opt3");
 var option4 = document.getElementById("opt4");
+var input1 = document.getElementById("ip1");
+var input2 = document.getElementById("ip2");
+var input3 = document.getElementById("ip3");
+var input4 = document.getElementById("ip4");
 var nextButton = document.getElementById("nextButton");
 var againButton = document.getElementById("againButton");
 var resultContainer = document.getElementById("result");
@@ -27,10 +31,18 @@ var correctQuestionCount = 0;
 function loadQuestion(questionIndex,i) {
     var q = questions[questionIndex];
     questionEl.textContent = (i+1) + '. ' +q.question;
-    opt1.textContent = q.option1;
-    opt2.textContent = q.option2;
-    opt3.textContent = q.option3;
-    opt4.textContent = q.option4;
+
+    option1.textContent = q.option1;
+    input1.value = q.option1;
+
+    option2.textContent = q.option2;
+    input2.value = q.option2;
+
+    option3.textContent = q.option3;
+    input3.value = q.option3;
+
+    option4.textContent = q.option4;
+    input4.value = q.option4;
 }
 
 function loadNextQuestion() {
@@ -40,6 +52,7 @@ function loadNextQuestion() {
         return;
     }
     var answer = selectedOption.value;
+    console.log(selectedOption.value);
     if (questions[currentQuestion].answer == answer) {
         this.score = this.score+1;
         this.correctQuestionCount = this.correctQuestionCount+1;
@@ -68,7 +81,7 @@ function loadNextQuestion() {
           ]);
 
         // Optional; add a title and set the width and height of the chart
-        var options = {'title':'Your Score: '+this.score+' out of '+totalQuestions, 'width':550, 'height':400};
+        var options = {'title':'Your Score: '+this.score+' out of '+totalQuestions, 'width':510, 'height':375,backgroundColor:'#333a44'};
 
         // Display the chart inside the <div> element with id="piechart"
         var chart = new google.visualization.PieChart(resultContainer);
