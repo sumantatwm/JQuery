ac261865587dbcd576073c2ae613aee998af248b
sumantatwm <sumantatwm@gmail.com>
2018-11-20 16:47:18 +0530

    new json

--- questions.js
+++ questions.js
@@ -4,33 +4,68 @@ var questions = [{
     "option2":"west",
     "option3":"North",
     "option4":"South",
-    "answer":"1"
+    "answer":"east"
 },{
     "question":"Sun sets in the ????",
     "option1":"east",
     "option2":"west",
     "option3":"North",
     "option4":"South",
-    "answer":"2"
+    "answer":"west"
 },{
     "question":"Shape of the Sun is ????",
     "option1":"Rectangle",
     "option2":"Square",
     "option3":"Round",
     "option4":"Triangle",
-    "answer":"3"
+    "answer":"Round"
 },{
     "question":"Color of the Sun is ????",
     "option1":"Yellow",
     "option2":"Red",
     "option3":"Pink",
     "option4":"Orange",
-    "answer":"4"
+    "answer":"Orange"
 },{
     "question":"Sun light is full of ????",
     "option1":"Vitamin-C",
     "option2":"Vitamin-B",
     "option3":"Vitamin-A",
     "option4":"Vitamin-E",
-    "answer":"3"
+    "answer":"Vitamin-A"
+},{
+    "question":"Father Of computer ????",
+    "option1":"Alan Turing",
+    "option2":"Cook",
+    "option3":"Root",
+    "option4":"Struat",
+    "answer":"Alan Turing"
+},{
+    "question":"Father Of telephone ????",
+    "option1":"Stokes",
+    "option2":"Cook",
+    "option3":"Root",
+    "option4":"Bell",
+    "answer":"Bell"
+},{
+    "question":"Father Of Electricity ????",
+    "option1":"cook",
+    "option2":"Farady",
+    "option3":"Root",
+    "option4":"Struat",
+    "answer":"Farady"
+},{
+    "question":"Father Of light ????",
+    "option1":"cook",
+    "option2":"Farady",
+    "option3":"Edition",
+    "option4":"Root",
+    "answer":"Edition"
+},{
+    "question":"Father Of gravity ????",
+    "option1":"Newton",
+    "option2":"Farady",
+    "option3":"Edition",
+    "option4":"Root",
+    "answer":"Newton"
 }]
